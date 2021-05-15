5663f08ef3e3a10078d972c57b9392960e21a72f
Ayarmz <johnayarmahe@gmail.com>
2021-05-15 12:20:53 +0000

    changed project structure

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 
-const api = require('./routes/api');
+const api = require('./apis/api');
 
 const PORT = 3000
 const app = express();
